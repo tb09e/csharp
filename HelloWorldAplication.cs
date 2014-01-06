9211b485ffe58e0e3c90ccd63514497a48246aeb
Toy <Bennett.Toy@gmail.com>
2014-01-06 15:59:56 -0500

    renamed app

---
+++ HelloWorldAplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+        }
+    }
+
+}
